c45d40119505b10ca4920ead799a87fe41749576
AngelYQ1 <angel.yucra.q@tecsup.edu.pe>
2025-05-19 19:53:14 -0500

    Pruebas Unitarias

--- test/pruebas/ArrayTest.java
+++ test/pruebas/ArrayTest.java
@@ -8,14 +8,33 @@ import static org.junit.jupiter.api.Assertions.*;
 public class ArrayTest {
     @Test
     public void testSize() {
+        int[] arr = {1,2,3};
+        Array a = new Array(arr);
+        assertEquals(3,a.size());
     }
 
     @Test
     public void testToString() {
+        int[] arr = {1,2,3};
+        Array a = new Array(arr);
+        assertEquals("1;2;3", a.toString());
     }
 
     @Test
     public void testSetElementAt() {
-    }
+        int[] arr = {1,2,3};
+        Array a = new Array(arr);
+        a.setElementAt(100,2);
 
+        int[] obt = a.getArray();
+        assertEquals(obt[2],100);
+
+        try {
+            a.setElementAt(100, -1);
+            assertFalse(true);
+        }
+        catch (Exception e) {
+            assertTrue(true);
+        }
+    }
 }

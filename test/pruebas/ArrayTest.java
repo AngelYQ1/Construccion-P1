ee3be5654c9d0bb9da19ab1dc5e799a554eef04a
AngelYQ1 <angel.yucra.q@tecsup.edu.pe>
2025-05-19 19:24:21 -0500

    Crear la clase de prueba

---
+++ test/pruebas/ArrayTest.java
@@ -0,0 +1,9 @@
+package pruebas;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ArrayTest {
+}
